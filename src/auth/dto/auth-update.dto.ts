d67cb461e83da7c45a39bcbc36620142e24944a5
weii1501 <vodacluc15@gmail.com>
2024-11-25 15:35:33 +0700

    feat: add get-profile feature

--- src/auth/dto/auth-update.dto.ts
+++ src/auth/dto/auth-update.dto.ts
@@ -1,13 +1,16 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
 import { FileDto } from '../../files/dto/file.dto';
-import { Transform } from 'class-transformer';
+import { Transform, Type } from 'class-transformer';
 import { lowerCaseTransformer } from '../../utils/transformers/lower-case.transformer';
+import { StatusDto } from '../../statuses/dto/status.dto';
+import { GenderEnum } from '../../genders/genders.enum';
+import { GenderDto } from '../../genders/dto/gender.dto';
 
 export class AuthUpdateDto {
-  @ApiPropertyOptional({ type: () => FileDto })
-  @IsOptional()
-  photo?: FileDto | null;
+  // @ApiPropertyOptional({ type: () => FileDto })
+  // @IsOptional()
+  // photo?: FileDto | null;
 
   @ApiPropertyOptional({ example: 'John' })
   @IsOptional()
@@ -26,14 +29,28 @@ export class AuthUpdateDto {
   @Transform(lowerCaseTransformer)
   email?: string;
 
+  @ApiPropertyOptional({ type: () => FileDto })
+  @IsOptional()
+  photo?: FileDto | null;
+
+  @ApiPropertyOptional({ example: { id: GenderEnum.other }, type: GenderDto })
+  @IsOptional()
+  @Type(() => GenderDto)
+  gender?: GenderDto | null;
+
+  @ApiPropertyOptional({ type: () => StatusDto })
+  @IsOptional()
+  @Type(() => StatusDto)
+  status?: StatusDto;
+
   @ApiPropertyOptional()
   @IsOptional()
-  @IsNotEmpty()
+  // @IsNotEmpty()
   @MinLength(6)
   password?: string;
 
   @ApiPropertyOptional()
   @IsOptional()
-  @IsNotEmpty({ message: 'mustBeNotEmpty' })
+  // @IsNotEmpty({ message: 'mustBeNotEmpty' })
   oldPassword?: string;
 }
