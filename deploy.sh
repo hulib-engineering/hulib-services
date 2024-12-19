2d9efc9200ba9e33aa1c8de27f945a81adbe829d
weii1501 <vodacluc15@gmail.com>
2024-12-19 14:26:55 +0700

    feat: test cicd on local

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+# In ra thời gian deploy
+echo "Starting deployment at $(date)"
+
+# 1. Di chuyển đến thư mục dự án (nếu cần)
+# cd hulib-services || exit
+
+# echo "Pulling latest code from repository..."
+# git pull || { echo "Failed pull"; exit 1; }
+
+echo "Stopping Docker images..."
+sudo docker-compose -f docker-compose.prod.yaml down || { echo "Failed to stop containers"; exit 1; }
+
+echo "Building Docker images..."
+sudo docker-compose -f docker-compose.prod.yaml build || { echo "Failed to build containers"; exit 1; }
+
+echo "Starting the application containers..."
+sudo docker-compose -f docker-compose.prod.yaml up -d || { echo "Failed to start containers"; exit 1; }
+
+# Hoàn thành deploy
+echo "Deployment completed successfully at $(date)"
