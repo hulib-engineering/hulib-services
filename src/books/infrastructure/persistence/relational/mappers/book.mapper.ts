5165e7335baee2f8c9c434432cee594d6d3be008
VuHoaBinh <hoabinh.vippro63@gmail.com>
2025-01-10 23:39:32 +0700

    feat: create new human books

--- src/books/infrastructure/persistence/relational/mappers/book.mapper.ts
+++ src/books/infrastructure/persistence/relational/mappers/book.mapper.ts
@@ -1,7 +1,1 @@
-import { Book } from '../../../../domain/book'; // Domain model
-import { BookEntity } from '../entities/book.entity'; // Persistence model
-import { UserMapper } from '../../../../../users/infrastructure/persistence/relational/mappers/user.mapper'; // Maps UserEntity <-> User
-import { Tag } from '../../../../../tags/domain/tag'; // Tag domain model
-import { TagEntity } from '../../../../../tags/infrastructure/persistence/relational/entities/tag.entity'; // Tag persistence model
-
 export class BooksMapper {}
