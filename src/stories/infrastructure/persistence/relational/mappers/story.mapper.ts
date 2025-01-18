54d7ace5bb35c5b57451064e02163399482419f8
NhatNam <namnhat1110@gmail.com>
2025-01-18 17:11:59 +0700

    feat: update story mapper to retrieve humanBook data

--- src/stories/infrastructure/persistence/relational/mappers/story.mapper.ts
+++ src/stories/infrastructure/persistence/relational/mappers/story.mapper.ts
@@ -1,3 +1,4 @@
+import { UserMapper } from '../../../../../users/infrastructure/persistence/relational/mappers/user.mapper';
 import { Story } from '../../../../domain/story';
 import { StoryEntity } from '../entities/story.entity';
 
@@ -7,6 +8,10 @@ export class StoryMapper {
     domainEntity.abstract = raw.abstract;
     domainEntity.title = raw.title;
     domainEntity.id = raw.id;
+    if (raw.humanBook) {
+      domainEntity.humanBook = UserMapper.toDomain(raw.humanBook);
+    }
+    domainEntity.rating = raw.rating;
     domainEntity.createdAt = raw.createdAt;
     domainEntity.updatedAt = raw.updatedAt;
 
@@ -20,6 +25,12 @@ export class StoryMapper {
     if (domainEntity.id) {
       persistenceEntity.id = domainEntity.id;
     }
+    if (domainEntity.humanBook) {
+      persistenceEntity.humanBook = UserMapper.toPersistence(
+        domainEntity.humanBook,
+      );
+    }
+    persistenceEntity.rating = domainEntity.rating;
     persistenceEntity.createdAt = domainEntity.createdAt;
     persistenceEntity.updatedAt = domainEntity.updatedAt;
 
