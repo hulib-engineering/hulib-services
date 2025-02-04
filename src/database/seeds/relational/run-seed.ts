b59114add8d062dd8b4027a8f1d95ca386a5076d
Truong Ma Phi <trmaphi@gmail.com>
2025-02-05 03:58:18 +0700

    chore: some seeds

--- src/database/seeds/relational/run-seed.ts
+++ src/database/seeds/relational/run-seed.ts
@@ -12,12 +12,12 @@ const runSeed = async () => {
   const app = await NestFactory.create(SeedModule);
 
   // run
-  // await app.get(GenderSeedService).run();
-  // await app.get(RoleSeedService).run();
-  // await app.get(StatusSeedService).run();
-  // await app.get(UserSeedService).run();
-  // await app.get(TopicSeedService).run();
-  await app.get(StorySeedService).run();
+  await app.get(GenderSeedService).run();
+  await app.get(RoleSeedService).run();
+  await app.get(StatusSeedService).run();
+  await app.get(UserSeedService).run();
+  await app.get(TopicSeedService).run();
+  // await app.get(StorySeedService).run();
   // await app.get(StoryReviewSeedService).run();
 
   await app.close();
