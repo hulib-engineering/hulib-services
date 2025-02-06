193dee27e2ff6d49ed438a6d7e370aac04743fd3
Truong Ma Phi <trmaphi@gmail.com>
2025-02-06 20:49:45 +0700

    chore: apply prisma post install and fix downtime when production deploy

--- deploy.sh
+++ deploy.sh
@@ -7,14 +7,8 @@ echo "Starting deployment at $(date)"
 echo "Pulling latest code from repository..."
 git pull || { echo "Failed pull"; exit 1; }
 
-echo "Stopping Docker images..."
-sudo docker-compose -f docker-compose.prod.yaml down || { echo "Failed to stop containers"; exit 1; }
-
-echo "Building Docker images..."
-sudo docker-compose -f docker-compose.prod.yaml build || { echo "Failed to build containers"; exit 1; }
-
 echo "Starting the application containers..."
-sudo docker-compose -f docker-compose.prod.yaml up -d || { echo "Failed to start containers"; exit 1; }
+sudo docker-compose -f docker-compose.prod.yaml up -d --build || { echo "Failed to start containers"; exit 1; }
 
 # Hoàn thành deploy
 echo "Deployment completed successfully at $(date)"
