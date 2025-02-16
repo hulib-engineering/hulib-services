3bdf89b480172a7cdaa5f3f0472cb32069ac96a8
Truong Ma Phi <trmaphi@gmail.com>
2025-02-16 08:40:39 +0700

    feat: add sentry monitor

---
+++ src/instrument.ts
@@ -0,0 +1,8 @@
+// Import with `const Sentry = require("@sentry/nestjs");` if you are using CJS
+import * as Sentry from '@sentry/nestjs';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [nodeProfilingIntegration()],
+});
