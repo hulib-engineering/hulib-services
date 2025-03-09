94be2b036d8558ba79ece3447ae34d1a9b12fab4
lequocduyquang <duyquangbtx@gmail.com>
2025-03-09 16:14:57 +0700

    chore: fix import casl module

--- src/reading-sessions/reading-sessions.module.ts
+++ src/reading-sessions/reading-sessions.module.ts
@@ -4,10 +4,12 @@ import { ReadingSession } from './entities/reading-session.entity';
 import { ReadingSessionParticipant } from './entities/reading-session-participant.entity';
 import { ReadingSessionsService } from './reading-sessions.service';
 import { ReadingSessionsController } from './reading-sessions.controller';
+import { CaslModule } from '@casl/casl.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ReadingSession, ReadingSessionParticipant]),
+    CaslModule,
   ],
   controllers: [ReadingSessionsController],
   providers: [ReadingSessionsService],
