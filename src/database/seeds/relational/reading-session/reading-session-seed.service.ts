c8266f2ec1ee6826fb219dbef00b03f2301258b2
lequocduyquang <duyquangbtx@gmail.com>
2025-03-22 11:01:48 +0700

    chore: update scheduleId

--- src/database/seeds/relational/reading-session/reading-session-seed.service.ts
+++ src/database/seeds/relational/reading-session/reading-session-seed.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { IsNull, Repository } from 'typeorm';
 import { faker } from '@faker-js/faker';
 
 // Domain Models
