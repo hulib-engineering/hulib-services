043b6014c880c0ed8654ebb458bf7f39b1a43e24
JtotheDoubleO <joo291998@gmail.com>
2025-04-29 21:20:45 +0700

    fix(File Driver): fixed uploader method temporarily

--- src/files/infrastructure/uploader/local/files.service.ts
+++ src/files/infrastructure/uploader/local/files.service.ts
@@ -28,12 +28,9 @@ export class FilesLocalService {
 
     return {
       file: await this.fileRepository.create({
-        path: `${this.configService.get('app.backendDomain', { infer: true })}/${this.configService.get(
-          'app.apiPrefix',
-          {
-            infer: true,
-          },
-        )}/v1/${file.path}`,
+        path: `/${this.configService.get('app.apiPrefix', {
+          infer: true,
+        })}/v1/${file.path}`,
       }),
     };
   }
