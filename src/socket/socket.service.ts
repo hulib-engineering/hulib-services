be225c86b13fb5183a3772f416cc0dc91eda5ab3
JtotheDoubleO <joo291998@gmail.com>
2025-06-06 15:21:29 +0700

    feat(317478): added redis to cache socket and initiated socket module

---
+++ src/socket/socket.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+
+import { SocketGateway } from './socket.gateway';
+
+import { CacheService } from '../cache/cache.service';
+
+@Injectable()
+export class SocketService {
+  constructor(
+    private readonly socketGateway: SocketGateway,
+    private readonly cacheService: CacheService,
+  ) {}
+
+  async sendTo(userId: string, event: string, data?: any) {
+    const userClients = await this.cacheService.get<string[]>({
+      key: 'UserSocketClients',
+      args: [userId],
+    });
+    if (userClients && Array.isArray(userClients)) {
+      for (const clientId of userClients) {
+        const client = this.socketGateway.getClient(clientId);
+        if (client) {
+          client.send(
+            JSON.stringify({
+              event,
+              data,
+            }),
+          );
+        }
+      }
+    }
+  }
+
+  sendToAll(event: string, data: any) {
+    const allClients = this.socketGateway.getAllClients();
+    for (const client of allClients.values()) {
+      client.send(
+        JSON.stringify({
+          event,
+          data,
+        }),
+      );
+    }
+  }
+}
