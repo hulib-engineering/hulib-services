c7e899b84b20a14d79de5fdc5e5a30bbcb0f2d66
JtotheDoubleO <joo291998@gmail.com>
2025-06-16 04:50:07 +0700

    refactor(Redis): configured redis docker production

--- redis-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-if [ -n "$REDIS_PASSWORD" ]; then
-  echo "requirepass $REDIS_PASSWORD" >> /usr/local/etc/redis/redis.conf
-fi
-
-exec redis-server /usr/local/etc/redis/redis.conf
