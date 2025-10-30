6d6fdc9ca8a4a3499f31969511c24b8f0be9ec24
JtotheDoubleO <joo291998@gmail.com>
2025-10-30 13:20:56 +0700

    fix: hot - fixed migration

--- prisma/migrations/20251028200157_add_rejected_reason_to_report/migration.sql
+++ prisma/migrations/20251028200157_add_rejected_reason_to_report/migration.sql
@@ -1,5 +1,5 @@
 -- AlterTable
 ALTER TABLE "report"
-    ADD COLUMN     "customReason" TEXT
+    ADD COLUMN     "customReason" TEXT,
     ADD COLUMN     "rejectedCustomReason" TEXT,
     ADD COLUMN     "rejectedReason" TEXT;
