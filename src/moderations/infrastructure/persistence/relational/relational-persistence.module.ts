784697463e95981ebe70e8437372aae17c2c7139
lequocduyquang <duyquangbtx@gmail.com>
2025-11-01 18:53:42 +0700

    fix: load entity and module

--- src/moderations/infrastructure/persistence/relational/relational-persistence.module.ts
+++ src/moderations/infrastructure/persistence/relational/relational-persistence.module.ts
@@ -3,9 +3,13 @@ import { ModerationRepository } from '../moderation.repository';
 import { ModerationRelationalRepository } from './repositories/relational-moderation.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ModerationEntity } from './entities/moderation.entity';
+import { ReportEntity } from '@reports/infrastructure/persistence/relational/entities/report.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ModerationEntity])],
+  imports: [TypeOrmModule.forFeature([
+    ModerationEntity,
+    ReportEntity,
+  ])],
   providers: [
     {
       provide: ModerationRepository,
--- src/moderations/moderations.module.ts
+++ src/moderations/moderations.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RelationalReportPersistenceModule } from './infrastructure/persistence/relational/relational-persistence.module';
 import { ModerationsController } from './moderations.controller';
 import { ModerationsService } from './moderations.service';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [RelationalReportPersistenceModule],
+  imports: [RelationalReportPersistenceModule, UsersModule],
   controllers: [ModerationsController],
   providers: [ModerationsService],
   exports: [ModerationsService, RelationalReportPersistenceModule],
