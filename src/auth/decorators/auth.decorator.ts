9ec8cfdc77908cebb71a8f9387b7d5b3bc631eb3
catdobyte <catdobyte@gmail.com>
2026-07-31 22:21:41 +0700

    fix: circular dêependency and casl package name

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -3,11 +3,11 @@ import { ApiBearerAuth } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
 import { RolesGuard } from '@roles/roles.guard';
 import { Roles } from '@roles/roles.decorator';
-import { CaslGuard } from '@casl/guards/casl.guard';
+import { CaslGuard } from '@permission/guards/casl.guard';
 import {
   AbilityHandler,
   CheckAbilities,
-} from '@casl/decorators/casl.decorator';
+} from '@permission/decorators/casl.decorator';
 
 export const AuthRoles = (...roles: number[]) =>
   applyDecorators(
